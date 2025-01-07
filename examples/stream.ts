a3659a5028ac7c42be56b95e41e089cddd368830
Sunil Pai <spai@cloudflare.com>
2025-01-07 15:53:40 +0000

    boop

--- examples/stream.ts
+++ examples/stream.ts
@@ -4,12 +4,6 @@ import { LanguageModelV1 } from "ai";
 import type { Browser } from "@cloudflare/puppeteer";
 
 export async function run(browser: Browser, llm: LanguageModelV1) {
-  // // Launch a browser instance
-  // const browser = await chromium.launch();
-
-  // // Initialize LLM provider
-  // const llm = openai.chat("gpt-4o");
-
   // Create a new LLMScraper
   const scraper = new LLMScraper(llm);
 
@@ -40,12 +34,5 @@ export async function run(browser: Browser, llm: LanguageModelV1) {
   await page.close();
   await browser.close();
 
-  // Stream the result from LLM
-  // for await (const data of stream) {
-  //   console.log(JSON.stringify(data.top, null, 2));
-  // }
-
-  // await page.close();
-  // await browser.close();
   return stream;
 }
